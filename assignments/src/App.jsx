6cd80bf015330399eb061c4e24e6679a2609aeb7
Harsh <harshpandeyat@gmail.com>
2024-10-14 01:56:57 +0530

    Completed Star Rating

--- assignments/src/App.jsx
+++ assignments/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Accordian from './components/accordian'
 import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
 
 const App = () => {
   return (
     <div>
     {/* <Accordian/> */}
-    <RandomColor/>
+    {/* <RandomColor/> */}
+    <StarRating/>
     </div>
   )
 }
